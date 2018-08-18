d9d34bb482b90f4dfb425ab4cc37e14a741326e9
Teodor Lazarov <tedylazarov@gmail.com>
2018-08-18 12:48:20 +0300

    Adding user input and checks

--- PrimeNumbers/src/PrimeNumbers.java
+++ PrimeNumbers/src/PrimeNumbers.java
@@ -1,18 +1,24 @@
+import java.util.Scanner;
+
 public class PrimeNumbers {
 
-    private static void PrintPrimes(int number) {
+    private static void PrintPrimes(int lower, int upper) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i <= number; i++) {
+        int temp = lower;
+        if(lower < 2){
+            lower = 2;
+        }
+        for (int i = lower; i <= upper; i++) {
             if (IsPrime(i)) {
                 sb.append(i);
                 sb.append(" ");
             }
         }
-        System.out.printf("Prime numbers from 0 to %d are: %n%s", number, sb.toString());
+        System.out.printf("Prime numbers from %d to %d are: %n%s", temp, upper, sb.toString());
     }
 
     private static boolean IsPrime(int n) {
-        for (int i = 2; i < n / 2; i++) {
+        for (int i = 2; i <= n / 2; i++) {
             if (n % i == 0) {
                 return false;
             }
@@ -20,7 +26,50 @@ public class PrimeNumbers {
         return true;
     }
 
+    private static void GetUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        int lower = 0, upper = 0;
+        boolean flag = true;
+        System.out.println("Enter a number range to be checked\nEnter lower bound:");
+
+        while (flag) {
+            try {
+                lower = Integer.parseInt(scanner.nextLine());
+                if (lower < 0) {
+                    System.out.println("Enter a valid positive integer!");
+                }
+                else{
+                    flag = false;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Enter a valid positive integer!");
+            }
+        }
+
+        flag = true;
+        System.out.println("Enter upper bound");
+        while (flag) {
+            try {
+                upper = Integer.parseInt(scanner.nextLine());
+                if (upper < 0) {
+                    System.out.println("Enter a valid positive integer!");
+                }
+                else if(upper < lower){
+                    System.out.println("Enter an upper value that is bigger than the lower value!");
+                }
+                else{
+                    flag = false;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Enter a valid positive integer!");
+            }
+        }
+
+        PrintPrimes(lower, upper);
+
+    }
+
     public static void main(String[] args) {
-        PrintPrimes(50);
+        GetUserInput();
     }
 }
